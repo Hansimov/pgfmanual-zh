308f77f36bddd0825f487f5f9453f9a958284bcb
Hansimov <591172499@qq.com>
2018-07-02 19:02:38 +0800

    + Succeed outputing pgfmanual-zh.pdf + Improve file structure + Add ctex support > To solve the `plot files not found problem`

---
+++ pgf-zh/rename_file.py
@@ -0,0 +1,14 @@
+# Replacing Filename characters with python
+#   https://stackoverflow.com/a/7161453/8328786
+
+import os
+
+folder='F:/Sources/git/pgfmanual-zh/pgf-zh/'
+pathiter = (os.path.join(root, filename)
+    for root, _, filenames in os.walk(folder)
+    for filename in filenames
+)
+for path in pathiter:
+    newname =  path.replace('-en-', '-zh-')
+    if newname != path:
+        os.rename(path,newname)

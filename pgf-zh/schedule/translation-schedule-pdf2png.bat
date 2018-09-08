9510ad0a7b0e83766eaa49e3de81472cf8ed9bed
Hansimov <591172499@qq.com>
2018-09-08 13:43:01 +0800

    + Finish guidelines

--- pgf-zh/schedule/translation-schedule-pdf2png.bat
+++ pgf-zh/schedule/translation-schedule-pdf2png.bat
@@ -2,5 +2,5 @@
 rem latexmk -pv -xelatex translation-schedule.tex
 rem latexmk -pv -pdf translation-schedule.tex
 echo Converting pdf to png ...
-F:/Technology/ImageMagick/convert -density 200 ./translation-schedule.pdf -background "#FFFFFF" -flatten ./translation-schedule.png
+D:/ImageMagick/convert -density 200 ./translation-schedule.pdf -background "#FFFFFF" -flatten ./translation-schedule.png
 rem pause
